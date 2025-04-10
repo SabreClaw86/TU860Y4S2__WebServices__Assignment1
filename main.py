e553abc4b4102ce434b23d7e07e672c05e0ce9b8
SabreClaw86 <114063362+SabreClaw86@users.noreply.github.com>
2025-04-10 13:34:18 +0100

    Update

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from typing import Union
 
 from fastapi.encoders import jsonable_encoder
 from fastapi.responses import JSONResponse
+from fastapi.openapi.utils import get_openapi
 from pymongo import MongoClient
 from fastapi import FastAPI, requests
 from pydantic import BaseModel
@@ -149,6 +150,29 @@ def convert_dollar_to_euro(product_id: str):
             return {"unit_price__euro": -1}
     except Exception as e:
         return {"error": str(e) }
+
+
+
+def custom_openapi():
+    if app.openapi_schema:
+        return app.openapi_schema
+    openapi_schema = get_openapi(
+        title="Fast API Auto Products",
+        version="2.5.0",
+        summary="This is a very custom OpenAPI schema",
+        description="Here's a longer description of the custom **OpenAPI** schema",
+        routes=app.routes,
+    )
+    openapi_schema["info"]["x-logo"] = {
+        "url": "https://fastapi.tiangolo.com/img/logo-margin/logo-teal.png"
+    }
+    app.openapi_schema = openapi_schema
+    return app.openapi_schema
+
+
+app.openapi = custom_openapi
+
+
 
 '''
 
